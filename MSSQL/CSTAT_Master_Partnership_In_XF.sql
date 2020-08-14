e486535cb6f8c573fda91a1f8efcca9b6274b598
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:04:51 +0530

    updated

--- MSSQL/CSTAT_Master_Partnership_In_XF.sql
+++ MSSQL/CSTAT_Master_Partnership_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Master Limited Partnership In XF.
+/***********************************************************************************************
+Returns Master Limited Partnership
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query finds Master Limited Partnerships (MLP) using Compustat data in Xpressfeed.
+This query finds Master Limited Partnerships (MLP) using Compustat data in Xpressfeed
 
 ***********************************************************************************************/
 
