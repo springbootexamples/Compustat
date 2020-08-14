2012c55efad436085dd4ebb08c3a8bf345f40a2a
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:03:51 +0530

    updated

--- MSSQL/CSTAT_Historic_Company_Tickers_In_XF.sql
+++ MSSQL/CSTAT_Historic_Company_Tickers_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Pulling Historic Company Tickers In XF
+/***********************************************************************************************
+Returns Historic Company Tickers
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns the Compustat historic company tickers from the Corporate Tracker package in Xpressfeed.
+This query returns the Compustat historic company tickers from the Corporate Tracker package in Xpressfeed
 
 ***********************************************************************************************/
 
