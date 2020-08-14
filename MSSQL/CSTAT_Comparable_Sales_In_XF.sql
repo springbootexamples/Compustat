c91aa5f7ca3704d34dda99dc8f836cfd3c2826de
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:02:32 +0530

    updated

--- MSSQL/CSTAT_Comparable_Sales_In_XF.sql
+++ MSSQL/CSTAT_Comparable_Sales_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Pulling Comparable Sales.
+/***********************************************************************************************
+Returns Comparable Sales
 
 Packages Required:
 Core
@@ -17,12 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns the details of the Comparable Sales (%) - Quarterly for a company in Xpressfeed.
+This query returns the details of the Comparable Sales (%) - Quarterly for a company in Xpressfeed
 
 ***********************************************************************************************/
 
