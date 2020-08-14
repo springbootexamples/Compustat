f5e71c8794ec895f54a6b3cecfc0f68d0b35c89b
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:06:45 +0530

    updated

--- MSSQL/CSTAT_Trading_Information_For_A_Securty_In_XF.sql
+++ MSSQL/CSTAT_Trading_Information_For_A_Securty_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Trading Information For A Security.
+/***********************************************************************************************
+Returns Trading Information For A Security
 
 Packages Required:
 Core
@@ -21,13 +21,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query  returns the lowest, highest, average, and total daily trading volume including the 
-number of trading days for Microsoft during 2002.
+This query  returns the lowest, highest, average, and total daily trading volume including the  number of trading days for Microsoft during 2002
 
 ***********************************************************************************************/
 
@@ -40,9 +39,9 @@ SELECT s.tic
 
 , AVG ( p.cshtrd ) AS [ average ] 
 
- , SUM ( p.cshtrd ) AS [ total ] 
+xa0, SUM ( p.cshtrd ) AS [ total ] 
 
- , COUNT ( p.cshtrd ) AS [ number of days ] 
+xa0, COUNT ( p.cshtrd ) AS [ number of days ] 
 
 
 FROM sec_dprc p, security s  
