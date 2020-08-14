c9ad9ff794f18fe01324723287b7b843c16deb9a
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:05:08 +0530

    updated

--- MSSQL/CSTAT_Non_Trading_Active_Companies_In_XF.sql
+++ MSSQL/CSTAT_Non_Trading_Active_Companies_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding nonTrading Active Companies.
+/***********************************************************************************************
+Returns NonTrading Active Companies
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns all companies that are not actively trading on any exchange, but are still active.
+This query returns all companies that are not actively trading on any exchange, but are still active
 
 ***********************************************************************************************/
 
