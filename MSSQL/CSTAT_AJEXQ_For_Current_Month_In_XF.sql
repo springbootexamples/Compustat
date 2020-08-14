30550d8dbb4c1d7c4a4b418b3ec2f07d4d3ca4cb
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:00:55 +0530

    updated

--- MSSQL/CSTAT_AJEXQ_For_Current_Month_In_XF.sql
+++ MSSQL/CSTAT_AJEXQ_For_Current_Month_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding AJEXQ For Currrent Month.
+/***********************************************************************************************
+Returns AJEXQ For Current Month
 
 Packages Required:
 Core
@@ -17,17 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query In Compustat Xpressfeed, AJEXQ is tied to quarterly fundamental data 
-availability so AJEXQ is not available until the fundamental data for that period is available. 
-You can find more current information in Compustat Xpressfeed utilizing the co_adjfact data group.
-Co_adjfact is organized with effective from and thru dates rather than specific data periods.
-Note: Uncomment the second query to return the adjustment factor as of a specific date returning only 
-one row of data.
+This query In Compustat Xpressfeed, AJEXQ is tied to quarterly fundamental data  availability so AJEXQ is not available until the fundamental data for that period is available.  You can find more current information in Compustat Xpressfeed utilizing the co_adjfact data group. Co_adjfact is organized with effective from and thru dates rather than specific data periods. Note: Uncomment the second query to return the adjustment factor as of a specific date returning only  one row of data
 
 ***********************************************************************************************/
 
