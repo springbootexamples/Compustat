4ba8b9ca9763351d5ea97bcdbeb752e306b62575
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:06:31 +0530

    updated

--- MSSQL/CSTAT_Segment_Items_Available_In_XF.sql
+++ MSSQL/CSTAT_Segment_Items_Available_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Segment Items.
+/***********************************************************************************************
+Returns Segment Items
 
 Packages Required:
 Core
@@ -17,12 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns a list of all segment items and their descriptions.
+This query returns a list of all segment items and their descriptions
 
 ***********************************************************************************************/
 
