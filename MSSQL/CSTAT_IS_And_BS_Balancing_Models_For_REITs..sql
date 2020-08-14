76243a0d1791ec57d7c5ea2406d064cbcd830411
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:04:08 +0530

    updated

--- MSSQL/CSTAT_IS_And_BS_Balancing_Models_For_REITs..sql
+++ MSSQL/CSTAT_IS_And_BS_Balancing_Models_For_REITs..sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Pulling IS And BS Balancing Models For REITs.
+/***********************************************************************************************
+Returns IS And BS Balancing Models For REITs
 
 Packages Required:
 Core
@@ -27,13 +27,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns the Income Statement (IS) balancing model for Real Estate Investment Trusts (REITs) in Xpressfeed.
-Note: Uncomment the second query to get the Balance Sheet (BS) balancing model for REITs in Xpressfeed.
+This query returns the Income Statement (IS) balancing model for Real Estate Investment Trusts (REITs) in Xpressfeed. Note: Uncomment the second query to get the Balance Sheet (BS) balancing model for REITs in Xpressfeed
 
 ***********************************************************************************************/
 
