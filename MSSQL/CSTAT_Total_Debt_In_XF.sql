f0ff480a875e29c271d0969539315fff61ce1646
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:06:41 +0530

    updated

--- MSSQL/CSTAT_Total_Debt_In_XF.sql
+++ MSSQL/CSTAT_Total_Debt_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Total Debt.
+/***********************************************************************************************
+Returns Total Debt
 
 Packages Required:
 Core
@@ -17,13 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns Debt in Current Liabilities (DLC), Debt-Total Long Term (DLTT), and 
-the sum of both items to calculate the Total Debt for IBM as of 12/31/2007.
+This query returns Debt in Current Liabilities (DLC), Debt-Total Long Term (DLTT), and  the sum of both items to calculate the Total Debt for IBM as of 12/31/2007
 
 ***********************************************************************************************/
 
