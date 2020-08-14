5f0d664bb603dce5cd3e36b34cec2a01f6241d6a
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:05:54 +0530

    updated

--- MSSQL/CSTAT_Product_Lines_For_A_Company.sql
+++ MSSQL/CSTAT_Product_Lines_For_A_Company.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding CSTAT Product Lines For A company.
+/***********************************************************************************************
+Returns Compustat Product Lines For A Company
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns all reported product lines for Georgia Pacific as of December 31, 2002.
+This query returns all reported product lines for Georgia Pacific as of December 31, 2002
 
 ***********************************************************************************************/
 
