f87d90a98917e0d325ce88a4ee4c766c3ae28d81
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:01:42 +0530

    updated

--- MSSQL/CSTAT_Canadian_Companies_By_Datadate_In_XF.sql
+++ MSSQL/CSTAT_Canadian_Companies_By_Datadate_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Canadian Companies.
+/***********************************************************************************************
+Returns Canadian Companies
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns Canadian companies by annual datadate.
+This query returns Canadian companies by annual datadate
 
 ***********************************************************************************************/
 
