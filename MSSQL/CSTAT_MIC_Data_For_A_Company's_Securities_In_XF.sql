5cd5c7e3db70ffdc9e144fb9b68795fde71429f5
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:04:38 +0530

    updated

--- MSSQL/CSTAT_MIC_Data_For_A_Company's_Securities_In_XF.sql
+++ MSSQL/CSTAT_MIC_Data_For_A_Company's_Securities_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Pulling MIC Data For A Company's Securities.
+/***********************************************************************************************
+Returns MIC Data For A Company's Securities
 
 Packages Required:
 Core
@@ -20,13 +20,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns the Market Identifier Code (MIC) and the Stock Exchange Code (EXCHG) for 
-all of a companys equity listings in Xpressfeed.
+This query returns the Market Identifier Code (MIC) and the Stock Exchange Code (EXCHG) for  all of a companys equity listings in Xpressfeed
 
 ***********************************************************************************************/
 
