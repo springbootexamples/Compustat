e3139cafeeeeffc5a17eb2dc3031c759f5a7d170
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:06:29 +0530

    updated

--- MSSQL/CSTAT_Security_Identifiers_For_All_Issues_And_All_Available_Dates_In_Snapshot_In_XF.sql
+++ MSSQL/CSTAT_Security_Identifiers_For_All_Issues_And_All_Available_Dates_In_Snapshot_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Security Identifiers for all issues and all dates in Snapshot
+/***********************************************************************************************
+Returns Security Identifiers For All Issues And All Dates In Snapshot
 
 Packages Required:
 Snapshot
@@ -17,13 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns the identifiers and the exchanges that were active for each issue 
-during specified times.
+This query returns the identifiers and the exchanges that were active for each issue  during specified times
 
 ***********************************************************************************************/
 
@@ -44,4 +43,4 @@ WHERE a . gvkey = '010846'
 
 ORDER BY a . iid , a . effdate 
 
- 
+xa0
