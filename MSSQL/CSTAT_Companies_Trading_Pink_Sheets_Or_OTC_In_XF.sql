cfcf0a2a2d46b294a3c4358802601faccf22b222
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:02:06 +0530

    updated

--- MSSQL/CSTAT_Companies_Trading_Pink_Sheets_Or_OTC_In_XF.sql
+++ MSSQL/CSTAT_Companies_Trading_Pink_Sheets_Or_OTC_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Companies Trading Pink Sheets or OTC
+/***********************************************************************************************
+Returns Companies Trading Pink Sheets Or OTC
 
 Packages Required:
 Core
@@ -18,12 +18,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query  returns all of the companies trading pink sheets or OTC.
+This query  returns all of the companies trading pink sheets or OTC
 
 ***********************************************************************************************/
 
