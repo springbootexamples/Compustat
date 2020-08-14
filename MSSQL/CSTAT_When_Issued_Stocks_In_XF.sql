b85b1a8120cf84cad617c97458b5a6f39f45791f
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:06:50 +0530

    updated

--- MSSQL/CSTAT_When_Issued_Stocks_In_XF.sql
+++ MSSQL/CSTAT_When_Issued_Stocks_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Pulling When Issued Stocks.
+/***********************************************************************************************
+Returns When Issued Stocks
 
 Packages Required:
 Core
@@ -17,12 +17,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query returns When Issued stocks in Xpressfeed.
+This query returns When Issued stocks in Xpressfeed
 
 ***********************************************************************************************/
 
