e9ec055ec6381a0b5440fc8e70861490b7946599
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-14 15:04:58 +0530

    updated

--- MSSQL/CSTAT_Mutual_Funds_In_XF.sql
+++ MSSQL/CSTAT_Mutual_Funds_In_XF.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Finding Mutual Funds.
+/***********************************************************************************************
+Returns Mutual Funds
 
 Packages Required:
 Core
@@ -23,13 +23,12 @@ Query_Version:
 V1
 
 Query_Added_Date:
-01\07\2020
+01/07/2020
 
 DatasetKey:
 8
 
-This query is used to screen Xpressfeed for closed-end funds only, search for SIC code of 6726, 
-Issue Type (TPCI) of 7 (Mutual or investment trust fund), and Net Asset Value (NAVM) is a value.
+This query is used to screen Xpressfeed for closed-end funds only, search for SIC code of 6726,  Issue Type (TPCI) of 7 (Mutual or investment trust fund), and Net Asset Value (NAVM) is a value
 
 ***********************************************************************************************/
 
