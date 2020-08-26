e807779439a301aca0ef87a274fe4c71a38c3c74
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-26 21:57:48 +0530

    updated

--- MSSQL/Pull_CSTAT_Shares_Outstanding_For_Time_Period.sql
+++ MSSQL/Pull_CSTAT_Shares_Outstanding_For_Time_Period.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Returns Shares Outstanding For a Time Period in Compustat
+/***********************************************************************************************
+Returns Shares Outstanding For A Time Period In Compustat
 
 Packages Required:
 Core
@@ -23,21 +23,19 @@ Query_Added_Date:
 DatasetKey:
 8
 
-The following sample query pulls the Common Shares Outstanding - Current (CSHOC) values, include From and 
-To dates (fromDate and toDate), for a company from the Security - Daily Pricing (sec_dprc) 
-table in Xpressfeed
+The following sample query pulls the Common Shares Outstanding - Current (CSHOC) values, include From and  To dates (fromDate and toDate), for a company from the Security - Daily Pricing (sec_dprc)  table in Xpressfeed
 
 ***********************************************************************************************/
 
 SELECT 
-	cshoc,
-	gvkey,
-	iid,
-	MAX ( dataDate ) AS ToDate,
-	MIN ( dataDate ) AS FromDate
+        cshoc,
+        gvkey,
+        iid,
+        MAX ( dataDate ) AS ToDate,
+        MIN ( dataDate ) AS FromDate
 FROM sec_dprc
 WHERE gvkey = '174301'
-	AND iid = '01'
+        AND iid = '01'
 
 GROUP BY cshoc, gvkey, iid
 ORDER BY MAX ( datadate ) DESC
