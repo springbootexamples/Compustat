c1bd837098ff9522ed7ccae63f9cfe6140c0fa5b
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-26 21:57:13 +0530

    updated

--- MSSQL/Calculating_CSTAT_Daily_Total_Return_Values.sql
+++ MSSQL/Calculating_CSTAT_Daily_Total_Return_Values.sql
@@ -1,4 +1,4 @@
-/************************************************************************************************
+/***********************************************************************************************
 Returns Compustat Daily Total Return Values
 
 Packages Required:
@@ -56,45 +56,45 @@ rn1.rowNumber,
 rn2.*
 from company c
 JOIN security s on s.gvkey = c.gvkey
-	and s.iid = c.priusa --Primary trading us security
+        and s.iid = c.priusa --Primary trading us security
 JOIN (
-	SELECT 
-	gvkey,
-	datadate,
-	iid,
-	ROW_NUMBER () 
-	OVER ( PARTITION BY gvkey, iid ORDER BY gvkey, iid, datadate ASC) AS rowNumber 
-	FROM sec_dprc
-	WHERE 1 = 1 
-	AND gvkey = @Gvkey 
+        SELECTxa0
+        gvkey,
+        datadate,
+        iid,
+        ROW_NUMBER () 
+        OVER ( PARTITION BY gvkey, iid ORDER BYxa0gvkey, iid, datadate ASC) AS rowNumber 
+        FROM sec_dprc
+        WHERE 1 = 1 
+        AND gvkey = @Gvkey 
 ) rn1 on rn1.gvkey = s.gvkey
-	and rn1.iid = s.iid
+        and rn1.iid = s.iid
 JOIN sec_dprc s1 on s1.gvkey = rn1.gvkey
-	and s1.iid = rn1.iid
-	and s1.datadate = rn1.datadate
+        and s1.iid = rn1.iid
+        and s1.datadate = rn1.datadate
 JOIN ( 
-	SELECT gvkey,
-	datadate,
-	iid,
-	ROW_NUMBER () OVER ( PARTITION BY gvkey, iid ORDER BY gvkey, iid, datadate ASC) AS rowNumber
-	FROM sec_dprc
-	WHERE 1 = 1 
-	AND gvkey = @Gvkey 
+        SELECT gvkey,
+        datadate,
+        iid,
+        ROW_NUMBER () OVER ( PARTITION BY gvkey, iid ORDER BYxa0gvkey, iid, datadate ASC) AS rowNumber
+        FROM sec_dprc
+        WHERE 1 = 1 
+        AND gvkey = @Gvkey 
 ) rn2 on rn2.gvkey = rn1.gvkey
-	and rn2.iid = rn1.iid
-	and rn2.rowNumber = rn1.rowNumber -1
+        and rn2.iid = rn1.iid
+        and rn2.rowNumber = rn1.rowNumber -1
 
 join sec_dprc s2 on s2.gvkey = rn2.gvkey
-	and s2.iid = rn2.iid
-	and s2.datadate = rn2.datadate
+        and s2.iid = rn2.iid
+        and s2.datadate = rn2.datadate
 
 join sec_dtrt t1 on t1.gvkey = s1.gvkey
-	and t1.iid = s1.iid	
-	and s1.datadate BETWEEN t1.datadate and isnull( t1.thrudate, '1/1/2900')
+        and t1.iid = s1.iid     
+        and s1.datadate BETWEEN t1.datadate and isnull( t1.thrudate, '1/1/2900')
 
 join sec_dtrt t2 on t2.gvkey = s2.gvkey
-	and t2.iid = s2.iid
-	and s2.datadate BETWEEN t2.datadate and isnull( t2.thrudate, '1/1/2900')
+        and t2.iid = s2.iid
+        and s2.datadate BETWEEN t2.datadate and isnull( t2.thrudate, '1/1/2900')
 
 where 1=1
 and c.gvkey = @gvkey 
