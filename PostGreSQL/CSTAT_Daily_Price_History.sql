79ffe4f80bf57a3153e1a6613595bda55f064d2c
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-26 21:59:53 +0530

    updated

--- PostGreSQL/CSTAT_Daily_Price_History.sql
+++ PostGreSQL/CSTAT_Daily_Price_History.sql
@@ -1,5 +1,5 @@
-/************************************************************************************************
-Returns daily price history
+/***********************************************************************************************
+Returns Daily Price History
 
 Packages Required:
 Core
@@ -26,21 +26,19 @@ Query_Added_Date:
 DatasetKey:
 8
 
-Because there is so much daily history, it is recomended to input a datadate range or the query 
-will take a long time to run. To pull the enitre history, remove and sec_dprc.datadate = 12/31/2008 
-from the query.
+Because there is so much daily history, it is recomended to input a datadate range or the query  will take a long time to run. To pull the enitre history, remove and sec_dprc.datadate = 12/31/2008  from the query
 
 ***********************************************************************************************/
 
 SELECT 
-	sec_dprc.gvkey,
-	sec_dprc.datadate,
-	sec_dprc.iid,
-	sec_dprc.prccd,
-	sec_dprc.ajexdi 
+        sec_dprc.gvkey,
+        sec_dprc.datadate,
+        sec_dprc.iid,
+        sec_dprc.prccd,
+        sec_dprc.ajexdi 
 
 FROM sec_dprc
 WHERE sec_dprc.gvkey = '012141' 
-	AND sec_dprc.datadate  = '12/31/2008'
+        AND sec_dprc.datadate  = '12/31/2008'
 
 ORDER BY sec_dprc.datadate DESC 
