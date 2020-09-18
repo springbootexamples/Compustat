9c6db5e72142c28e3e4b9ff339a81aa1f9cbb46e
DipsikhaSarkar <dipsikha.sarkar@spglobal.com>
2020-09-18 22:09:47 +0530

    Queries edited and added from 'To Review' Folder
    Queries edited and added from 'To Review' Folder

---
+++ MSSQL/Calculates_CSTAT_Semi_Annual_Cah_Flow_For_International_Companies.sql
@@ -0,0 +1,199 @@
+/***********************************************************************************************
+Returns Semi-Annual Cah Flow For International Companies
+
+Packages Required:
+Core
+
+Universal Identifiers:
+GVKEY
+
+Primary Columns Used:
+consol
+datadate
+datafmt
+fyr
+gvkey
+indfmt
+popsrc
+
+Database_Type:
+MSSQL
+
+Query_Version:
+V1
+
+Query_Added_Date:
+01/07/2020
+
+DatasetKey:
+8
+
+The following sample query calculates semi-annual cash flow from operating activities (OANCFY) for international companies
+
+***********************************************************************************************/
+
+DECLARE @gvkey AS VARCHAR ( 6 ) SET @gvkey = '016603' --First query subtracts Q2 ytd from Q4 ytd to get second half cash flow:
+
+SELECT ( y4.oancfy-y2.oancfy ) oancfsa
+
+, curcdq
+
+, d.gvkey
+
+, d.datadate
+
+, d.fyr
+
+, d.datafmt
+
+, d.scfq
+
+, d.pdytd
+
+, d.fqtr
+
+
+FROM co_idesind d
+
+
+JOIN co_ifndytd y4 --cash flow data from fqtr 4 
+
+ON y4.gvkey = d.gvkey
+
+
+AND CAST(y4.datadate AS DATE) = CAST(d.datadate AS DATE)
+
+AND y4.indfmt = d.indfmt
+
+AND y4.consol = d.consol
+
+AND y4.popsrc = d.popsrc
+
+AND y4.fyr = d.fyr
+
+AND y4.datafmt = d.datafmt
+
+AND y4.oancfy IS NOT NULL
+
+JOIN co_ifndytd y2 --cash flow data from fqtr 2 
+
+ON y2.gvkey = d.gvkey
+
+AND y2.indfmt = d.indfmt
+
+AND y2.consol = d.consol
+
+AND y2.popsrc = d.popsrc
+
+AND y2.fyr = d.fyr
+
+AND y2.datafmt = d.datafmt
+
+AND y2.datadate = DATEADD ( mm, -6, y2.datadate )
+
+AND y2.oancfy IS NOT NULL
+
+JOIN co_ifntytd yf --cash flow footnotes 
+
+ON yf.gvkey = d.gvkey
+
+AND yf.datadate = d.datadate
+
+AND yf.indfmt = d.indfmt
+
+AND yf.consol = d.consol
+
+AND yf.popsrc = d.popsrc
+
+AND yf.fyr = d.fyr
+
+AND yf.datafmt = d.datafmt
+
+WHERE d.gvkey = @gvkey
+
+AND d.popsrc = 'i'
+
+AND d.datafmt = 'hist_std'
+
+AND d.fqtr = 4 --4th quarter only
+
+AND d.pdytd = 12 --12 month values only
+
+
+UNION
+
+--Second query adds in Q2 ytd as is:
+
+SELECT y2.oancfy oancfsa
+
+, curcdq
+
+, d.gvkey
+
+, d.datadate
+
+, d.fyr
+
+, d.datafmt
+
+, d.scfq
+
+, d.pdytd
+
+, d.fqtr
+
+
+FROM co_idesind d
+
+JOIN co_ifndytd y2 --cash flow data from fqtr 2 
+
+ON y2.gvkey = d.gvkey
+
+
+AND y2.indfmt = d.indfmt
+
+AND y2.consol = d.consol
+
+AND y2.popsrc = d.popsrc
+
+AND y2.fyr = d.fyr
+
+AND y2.datafmt = d.datafmt
+
+AND CAST(y2.datadate AS DATE) = CAST( d.datadate AS DATE)
+
+AND y2.oancfy IS NOT NULL
+
+
+JOIN co_ifntytd yf --cash flow footnotes 
+
+ON yf.gvkey = d.gvkey
+
+
+AND yf.datadate = d.datadate
+
+AND yf.indfmt = d.indfmt
+
+AND yf.consol = d.consol
+
+AND yf.popsrc = d.popsrc
+
+AND yf.fyr = d.fyr
+
+AND yf.datafmt = d.datafmt
+
+
+
+WHERE d.gvkey = @gvkey
+
+
+AND d.popsrc = 'i'
+
+AND d.datafmt = 'hist_std'
+
+AND d.fqtr = 2 --4th quarter only
+
+AND d.pdytd = 6 --12 month values only
+
+
+ORDER BY datadate DESC
